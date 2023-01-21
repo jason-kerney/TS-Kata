dc23a1b593762c9d655a3be857eaba29704e617f
JasonEdf <jason.kerney@edf-re.com>
2023-01-20 17:32:12 -0800

    * setting up testing environment

---
+++ specs/smoke.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe('A test suite to determine if the testing environment works', () => {
+    it('should know true is true', () => {
+        expect(true).to.be.false; // Whoops fix this line to make it pass.
+    });
+});
