972ed07c55d997494ba49d801f98e7e7327ab740
JasonEdf <jason.kerney@edf-re.com>
2023-01-20 17:09:34 -0800

    * setting up test environment

---
+++ spec/smoke.spec.ts
@@ -0,0 +1,5 @@
+describe('a test suite to know if the testing framework works', () => {
+    it('should know that true is true', () => {
+        expect(true).toBe(false); // Whoops, fix this to make things pass.
+    });
+});
