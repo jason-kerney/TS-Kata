02472d933810282ee4d78d8a45988da0ab40260a
JasonEdf <jason.kerney@edf-re.com>
2023-01-23 08:17:26 -0800

    * setting up test framework

---
+++ tests/smoke.test.ts
@@ -0,0 +1,5 @@
+describe('a test suite to know if the testing framework works', () => {
+    test('should know that true is true', () => {
+        expect(true).toEqual(false); // Whoops, fix this to make things pass.
+    });
+});
