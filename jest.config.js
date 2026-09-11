f99ca27906b81832c493f004f80fdecce27feddc
Jason Kerney <Jason@TeamKerney.com>
2026-09-11 12:03:58 -0700

    chore: update configuration files for improved TypeScript and Jest integration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('jest').Config} */
 module.exports = {
-    preset: 'ts-jest',
     testEnvironment: 'node',
+    transform: {
+        '^.+\\.tsx?$': ['@swc/jest'],
+    },
 };
